d93f978aa33bd19d8e4dac29921aac405dd352a2
total16 <daka0423@126.com>
2023-06-29 03:24:57 +0800

    use react-icons instead of mui

--- src/components/Common/SidebarButton.jsx
+++ src/components/Common/SidebarButton.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import './SidebarButton.css'
+import React from 'react';
+import { VscAccount } from 'react-icons/vsc';
+import './SidebarButton.css';
 
 function SidebarButton(props) {
 
     return (
         <div className='sidebar-button'>
-            
+            <VscAccount className='icon'/>
         </div>
     )
 }
